16f4ce38342fe0f4a1b47adfb2e310898a58cccb
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-07-02 20:50:39 +0100

    Fix hydration warning
    Advised in shadcn docs.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <Script
           src="https://unpkg.com/@elevenlabs/convai-widget-embed"
