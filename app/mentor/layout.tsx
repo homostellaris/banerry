6791208614504d1ef2c398383b4cf8bccbe32de8
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-07-11 21:43:47 +0100

    Bit of random cleanup

---
+++ app/mentor/layout.tsx
@@ -0,0 +1,18 @@
+import type React from "react";
+import Script from "next/script";
+import { PropsWithChildren } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <Script
+        src="https://unpkg.com/@elevenlabs/convai-widget-embed"
+        async
+        type="text/javascript"
+      ></Script>
+      {/* @ts-ignore */}
+      <elevenlabs-convai agent-id="agent_01jxr34ba0esfs9gp69ar7b3vq"></elevenlabs-convai>
+      {children}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function HomePage() {
   return (
     <>
       <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + Next.js + Convex Auth
         <SignOutButton />
       </header>
       <main>
