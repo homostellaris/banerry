cb408e87b5dfe390b5c9b0fc6a910496fc059d4c
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-07-21 21:40:19 +0100

    Fix home page right padding

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function HomePage() {
   return (
     <>
       <main>
-        <div className="flex flex-col min-h-screen items-center justify-center p-6 text-center pr-20">
+        <div className="flex flex-col min-h-screen items-center justify-center p-6 text-center">
           <h1 className="text-5xl font-bold text-purple-700 mb-6">Banerry</h1>
           <p className="text-2xl text-gray-600 max-w-2xl mb-10">
-            Communication assistance for gestalt language processors.
+            Communication assistance for gestalt language processors
           </p>
 
           <div className="space-y-4">
