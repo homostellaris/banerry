66128f78845715d713d6f15c1d41cf0eeba94dc1
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-08-03 13:05:33 +0100

    🐛 Having to login after every session

--- middleware.ts
+++ middleware.ts
@@ -7,14 +7,17 @@ import {
 const isSignInPage = createRouteMatcher(["/signin"]);
 const isProtectedRoute = createRouteMatcher(["/mentor", "/mentor/(.*)"]);
 
-export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-  if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/mentor");
-  }
-  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/signin");
-  }
-});
+export default convexAuthNextjsMiddleware(
+  async (request, { convexAuth }) => {
+    if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
+      return nextjsMiddlewareRedirect(request, "/mentor");
+    }
+    if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+      return nextjsMiddlewareRedirect(request, "/signin");
+    }
+  },
+  { cookieConfig: { maxAge: 60 * 60 * 24 * 30 } }
+);
 
 export const config = {
   // The following matcher runs middleware on all routes
