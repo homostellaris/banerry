7e1e5b6dc93754fa1f8e5d732ca2e3d4e733dadd
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2026-02-05 23:16:06 +0000

    📈 Switch to cookieless tracking

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,15 @@
-import posthog from "posthog-js";
+import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: "2025-11-30",
-  capture_exceptions: true,
-  debug: process.env.NODE_ENV === "development",
-});
+	api_host: '/ingest',
+	capture_performance: {
+		web_vitals: false, // Doesn't work with cookieless mode
+	},
+	cookieless_mode: 'always',
+	ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+	defaults: '2025-11-30',
+	capture_exceptions: true,
+	debug: process.env.NODE_ENV === 'development',
+})
+
+console.debug('instrumentation client loaded')
